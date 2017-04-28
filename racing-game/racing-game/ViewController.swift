e24359b57da69f0d66472213ad78ae8b7f5acbe0
aducret <argentino.ducret@wolox.com.ar>
2017-04-28 12:30:46 -0300

    Base tutorial

---
+++ racing-game/racing-game/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SpriteKit Introduction
+//
+//  Created by Davis Allie on 9/04/16.
+//  Copyright © 2016 tutsplus. All rights reserved.
+//
+
+import UIKit
+
+enum ButtonDirection: Int {
+    case Left = 0, Right = 1
+}
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func didPressButton(sender: UIButton) {
+        
+    }
+    
+}
+
